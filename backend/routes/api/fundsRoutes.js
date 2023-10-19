c6255284da84b536b53c4283052dbe52de1fe98c
marcusawd <angweidemarcus@gmail.com>
2023-10-20 00:29:21 +0800

    feat: Patch fund

--- backend/routes/api/fundsRoutes.js
+++ backend/routes/api/fundsRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const fundsCtrl = require("../../controllers/api/fundsCtrl");
 
 router.post("/", fundsCtrl.create);
+router.get("/:fundId", fundsCtrl.getOne);
+router.patch("/:fundId", fundsCtrl.patchOne);
 
 module.exports = router;
