090c9091f0b9ff939dfff876ac7a5a7c85b4b8da
marcusawd <angweidemarcus@gmail.com>
2023-10-27 10:36:32 +0800

    fix: Forgot to add a return

--- frontend/src/components/Fund/SelectTickerList.jsx
+++ frontend/src/components/Fund/SelectTickerList.jsx
@@ -1,22 +1,24 @@
 import { Button, Col, Form, ListGroup } from "react-bootstrap";
 
 export default function SelectTickerList({ tickers, handleAddTicker }) {
-	<Col>
-		<Form.Group controlId="selectTicker">
-			<Form.Label>Select Ticker:</Form.Label>
-			<ListGroup>
-				{tickers?.map((ticker) => (
-					<ListGroup.Item key={ticker}>
-						{ticker}
-						<Button
-							variant="primary"
-							className="ms-2"
-							onClick={() => handleAddTicker(ticker)}>
-							+
-						</Button>
-					</ListGroup.Item>
-				))}
-			</ListGroup>
-		</Form.Group>
-	</Col>;
+	return (
+		<Col>
+			<Form.Group controlId="selectTicker">
+				<Form.Label>Select Ticker:</Form.Label>
+				<ListGroup>
+					{tickers?.map((ticker) => (
+						<ListGroup.Item key={ticker}>
+							{ticker}
+							<Button
+								variant="primary"
+								className="ms-2"
+								onClick={() => handleAddTicker(ticker)}>
+								+
+							</Button>
+						</ListGroup.Item>
+					))}
+				</ListGroup>
+			</Form.Group>
+		</Col>
+	);
 }
--- frontend/src/components/Fund/SelectedTickersTable.jsx
+++ frontend/src/components/Fund/SelectedTickersTable.jsx
@@ -7,53 +7,55 @@ export default function SelectedTickersTable({
 	totalWeightage,
 	totalWeightageStyle,
 }) {
-	<Col xs={12} sm={8}>
-		<Form.Group controlId="selectedTickers">
-			<Form.Label>Selected Tickers:</Form.Label>
-			<Table striped bordered hover>
-				<thead>
-					<tr>
-						<th style={{ width: "5%" }}>#</th>
-						<th style={{ width: "40%" }}>Ticker</th>
-						<th style={{ width: "15%" }}>Weightage</th>
-						<th style={{ width: "15%" }}>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{Object.keys(selectedTickers)?.map((ticker, index) => (
-						<tr key={ticker}>
-							<td>{index + 1}</td>
-							<td>{ticker}</td>
-							<td>
-								<Form.Control
-									type="number"
-									value={selectedTickers[ticker]}
-									min={0}
-									onChange={(e) =>
-										setSelectedTickers({
-											...selectedTickers,
-											[ticker]: e.target.value,
-										})
-									}
-								/>
-							</td>
-							<td>
-								<Button
-									variant="danger"
-									onClick={() => handleRemoveTicker(ticker)}>
-									Remove
-								</Button>
-							</td>
+	return (
+		<Col xs={12} sm={8}>
+			<Form.Group controlId="selectedTickers">
+				<Form.Label>Selected Tickers:</Form.Label>
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th style={{ width: "5%" }}>#</th>
+							<th style={{ width: "40%" }}>Ticker</th>
+							<th style={{ width: "15%" }}>Weightage</th>
+							<th style={{ width: "15%" }}>Actions</th>
 						</tr>
-					))}
-					<tr>
-						<td></td>
-						<td>Total Weightage:</td>
-						<td style={totalWeightageStyle}>{totalWeightage}%</td>
-						<td></td>
-					</tr>
-				</tbody>
-			</Table>
-		</Form.Group>
-	</Col>;
+					</thead>
+					<tbody>
+						{Object.keys(selectedTickers)?.map((ticker, index) => (
+							<tr key={ticker}>
+								<td>{index + 1}</td>
+								<td>{ticker}</td>
+								<td>
+									<Form.Control
+										type="number"
+										value={selectedTickers[ticker]}
+										min={0}
+										onChange={(e) =>
+											setSelectedTickers({
+												...selectedTickers,
+												[ticker]: e.target.value,
+											})
+										}
+									/>
+								</td>
+								<td>
+									<Button
+										variant="danger"
+										onClick={() => handleRemoveTicker(ticker)}>
+										Remove
+									</Button>
+								</td>
+							</tr>
+						))}
+						<tr>
+							<td></td>
+							<td>Total Weightage:</td>
+							<td style={totalWeightageStyle}>{totalWeightage}%</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</Table>
+			</Form.Group>
+		</Col>
+	);
 }
