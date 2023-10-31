f984f5563c40d76ef1312d3441a738417da1c59b
marcusawd <angweidemarcus@gmail.com>
2023-10-31 11:05:25 +0800

    feat: Funds, transfers nav

--- frontend/src/components/NavBar/UserNavBar.jsx
+++ frontend/src/components/NavBar/UserNavBar.jsx
@@ -9,19 +9,19 @@ export default function UserNavBar({ user, handleLogout }) {
 				<Navbar.Text>Welcome back, {user.name}!</Navbar.Text>
 			</Navbar.Collapse>
 			<NavDropdown
-				title="Courses"
+				title="Funds"
 				id="basic-nav-dropdown"
 				style={{ marginRight: "30px" }}>
-				<NavDropdown.Item onClick={() => navigate("/courses")}>
-					Browse Courses
+				<NavDropdown.Item onClick={() => navigate("/funds")}>
+					Fund List
 				</NavDropdown.Item>
-				<NavDropdown.Item onClick={() => navigate("/courses/create")}>
+				{/* <NavDropdown.Item onClick={() => navigate("/courses/create")}>
 					Create A Course
-				</NavDropdown.Item>
+				</NavDropdown.Item> */}
 			</NavDropdown>
 			<NavDropdown title="Your Account" id="basic-nav-dropdown">
-				<NavDropdown.Item onClick={() => navigate("/profile")}>
-					Profile
+				<NavDropdown.Item onClick={() => navigate("/transfers")}>
+					Transfers
 				</NavDropdown.Item>
 				<NavDropdown.Divider />
 				<NavDropdown.Item
