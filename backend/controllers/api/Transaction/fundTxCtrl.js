904819285709e20629dc64a38c708ad4ec0ad7f3
marcusawd <angweidemarcus@gmail.com>
2023-10-31 17:08:25 +0800

    feat: Tx

---
+++ backend/controllers/api/Transaction/fundTxCtrl.js
@@ -0,0 +1,8 @@
+const pool = require("../../../config/database");
+const debug = require("debug")("backend:fundTxCtrl");
+
+const buyFund = async (req, res) => {};
+
+const sellFund = async (req, res) => {};
+
+module.exports = { buyFund, sellFund };
--- backend/controllers/api/Transaction/transferCtrl.js
+++ backend/controllers/api/Transaction/transferCtrl.js
@@ -1,36 +1,39 @@
 const pool = require("../../../config/database");
 const debug = require("debug")("backend:transferCtrl");
 
+const transferQuery = `WITH user_data AS (
+                        SELECT user_id FROM users WHERE email = $1),
+                        tx_type_data AS (
+                        SELECT tx_type_id FROM transaction_type WHERE tx_name = $2)
+                      INSERT INTO transactions (user_id, tx_type_id, amount)
+                      VALUES (
+                        (SELECT user_id FROM user_data),
+                        (SELECT tx_type_id FROM tx_type_data),
+                        $3);`;
+
 const deposit = async (req, res) => {
 	const TRANSACTION_NAME = "Deposit";
-	let { deposit } = req.body;
+	const { deposit } = req.body;
 	const { email } = req.user;
 	try {
-		const query = {
-			text: `WITH user_data AS (
-              SELECT user_id FROM users WHERE email = $1),
-              tx_type_data AS (
-              SELECT tx_type_id FROM transaction_type WHERE tx_name = $2)
-            INSERT INTO transactions (user_id, tx_type_id, amount)
-            VALUES (
-              (SELECT user_id FROM user_data),
-              (SELECT tx_type_id FROM tx_type_data),
-              $3);`,
-			values: [email, TRANSACTION_NAME, deposit],
-		};
-		await pool.query(query);
+		await pool.query(transferQuery, [email, TRANSACTION_NAME, deposit]);
 		res.status(201).json({ message: "Deposit success" });
 	} catch (error) {
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-	debug(email);
 };
 
 const withdraw = async (req, res) => {
 	const TRANSACTION_NAME = "Withdrawal";
 	let { withdraw } = req.body;
-	debug(withdraw);
-	res.json({ withdraw });
+	withdraw = -1 * Number(withdraw);
+	const { email } = req.user;
+	try {
+		await pool.query(transferQuery, [email, TRANSACTION_NAME, withdraw]);
+		res.status(201).json({ message: "Withdraw success" });
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 module.exports = { deposit, withdraw };
