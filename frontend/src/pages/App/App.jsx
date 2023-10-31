8f0f565f2b6fb719a095991a172e9e7f6b5c22ce
marcusawd <angweidemarcus@gmail.com>
2023-10-31 22:28:39 +0800

    feat: Footer

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -25,30 +25,72 @@ export default function App() {
 	log(user);
 
 	return (
-		<main className="App">
-			<NavBar user={user} />
-			<Routes>
-				{user && user.role === "admin" ? (
-					<>
-						<Route path="/admin" element={<AdminDashboardPage />}></Route>
-						<Route path="/admin/tickers" element={<AllTickersPage />} />
-						<Route path="/admin/fund/create" element={<CreateFundPage />} />
-					</>
-				) : null}
+		<>
+			<main className="App">
+				<NavBar user={user} />
+				<Routes>
+					{user && user.role === "admin" ? (
+						<>
+							<Route path="/admin" element={<AdminDashboardPage />}></Route>
+							<Route path="/admin/tickers" element={<AllTickersPage />} />
+							<Route path="/admin/fund/create" element={<CreateFundPage />} />
+						</>
+					) : null}
 
-				{user && user.role === "user" ? (
-					<>
-						<Route path="/profile" element={<UserDashboardPage />} />
-						<Route path="/transfers" element={<TransfersPage />} />
-					</>
-				) : null}
+					{user && user.role === "user" ? (
+						<>
+							<Route path="/profile" element={<UserDashboardPage />} />
+							<Route path="/transfers" element={<TransfersPage />} />
+						</>
+					) : null}
 
-				<Route path="/*" element={<LandingPage />} />
-				<Route path="/funds" element={<AllFundsPage />} />
-				<Route path="/funds/:fundName" element={<OneFundPage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/signup" element={<SignupPage />} />
-			</Routes>
-		</main>
+					<Route path="/*" element={<LandingPage />} />
+					<Route path="/funds" element={<AllFundsPage />} />
+					<Route path="/funds/:fundName" element={<OneFundPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/signup" element={<SignupPage />} />
+				</Routes>
+			</main>
+			<br />
+			<hr />
+			<footer className="text-center text-dark text-opacity-50">
+				<small>
+					FundRiser&trade; (est 2023)
+					<br />A project by{" "}
+					<a
+						className="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+						href="https://github.com/marcusawd">
+						marcusawd
+					</a>
+					<br />
+					<br />
+					Check out my other projects:
+					<br />
+					<a
+						className="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+						href="https://github.com/marcusawd/StockMarketWatch">
+						StockMarketWatch GitHub
+					</a>{" "}
+					|{" "}
+					<a
+						className="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+						href="https://stock-market-watch-jxfbi5e0c-marcusawd.vercel.app/">
+						StockMarketWatch Website
+					</a>
+					<br />
+					<a
+						className="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+						href="https://github.com/marcusawd/FinCademy">
+						FinCademy GitHub
+					</a>{" "}
+					|{" "}
+					<a
+						className="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+						href="https://fincademy.onrender.com/">
+						FinCademy Website
+					</a>
+				</small>
+			</footer>
+		</>
 	);
 }
