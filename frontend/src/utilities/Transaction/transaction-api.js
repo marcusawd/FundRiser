9d9335e706cec580090df8f8e3e54ca8c2626f96
marcusawd <angweidemarcus@gmail.com>
2023-10-31 11:03:19 +0800

    feat: Deposit

--- frontend/src/utilities/Transaction/transaction-api.js
+++ frontend/src/utilities/Transaction/transaction-api.js
@@ -3,11 +3,19 @@ import sendRequest from "../sendRequest";
 const BASE_URL = "/api/transaction";
 
 export async function depositFunds(amount) {
-	const message = await sendRequest(BASE_URL, "POST", amount);
+	const message = await sendRequest(
+		`${BASE_URL}/transfer/deposit`,
+		"POST",
+		amount,
+	);
 	return message;
 }
 
 export async function withdrawFunds(amount) {
-	const message = await sendRequest(BASE_URL, "POST", amount);
+	const message = await sendRequest(
+		`${BASE_URL}/transfer/withdraw`,
+		"POST",
+		amount,
+	);
 	return message;
 }
