87efd7ca79c6a37a2c2b938ccd8f1692517a70c6
marcusawd <angweidemarcus@gmail.com>
2023-11-02 01:31:02 +0800

    feat: sell api post

--- backend/controllers/api/Transaction/fundTxCtrl.js
+++ backend/controllers/api/Transaction/fundTxCtrl.js
@@ -13,10 +13,18 @@ const calculateBalance = (data) => {
 	return total;
 };
 
+const calculateShares = (data) => {
+	let total = 0;
+	data.forEach((tx) => {
+		total += tx.share_count;
+	});
+	return total;
+};
 const buyFund = async (req, res) => {
 	const TRANSACTION_NAME = "Buy Fund";
 	const { user_id } = req.user;
-	const { fundName, shareCount, amount } = req.body;
+	const { fundName, shareCount } = req.body;
+	const amount = parseFloat(req.body.amount);
 	const client = await pool.connect();
 	try {
 		client.query("BEGIN");
@@ -49,7 +57,35 @@ const buyFund = async (req, res) => {
 const sellFund = async (req, res) => {
 	const TRANSACTION_NAME = "Sell Fund";
 	const { user_id } = req.user;
-	const { fundName, shareCount, amount } = req.body;
+	const { fundName, shareCount } = req.body;
+	const amount = parseFloat(req.body.amount);
+	const client = await pool.connect();
+	try {
+		client.query("BEGIN");
+		const history = await client.query(getHistoryQuery, [user_id]);
+		const currentShares = calculateShares(history.rows);
+		if (currentShares < shareCount) {
+			throw new Error("shares");
+		}
+		await client.query(fundtxQuery, [
+			user_id,
+			TRANSACTION_NAME,
+			amount,
+			fundName,
+			shareCount,
+		]);
+		res.json({
+			message: `${shareCount} shares of ${fundName} sold for $${amount}`,
+		});
+		client.query("COMMIT");
+	} catch (error) {
+		if (error.message === "shares") {
+			res.status(400).json({ error: "Not enough shares to sell" });
+		}
+		client.query("ROLLBACK");
+	} finally {
+		client.release();
+	}
 };
 
 module.exports = { buyFund, sellFund };
