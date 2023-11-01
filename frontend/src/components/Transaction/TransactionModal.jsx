07d5ad31bcdb62a23d4879bf5db76b8d3291f898
marcusawd <angweidemarcus@gmail.com>
2023-11-01 17:48:55 +0800

    feat: More logic + css

--- frontend/src/components/Transaction/TransactionModal.jsx
+++ frontend/src/components/Transaction/TransactionModal.jsx
@@ -1,11 +1,48 @@
+import { useContext, useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import { UserContext } from "../../hooks/UserProvider";
+
+import debug from "debug";
+const log = debug("frontend:TranscationModal");
 
 export default function TransactionModal({
 	show,
 	handleClose,
 	purchase,
 	fundName,
+	fund,
 }) {
+	const { txHistory, setFetched } = useContext(UserContext);
+	const [balance, setBalance] = useState(0);
+	const [shareCount, setShareCount] = useState(0);
+	const [quantity, setQuantity] = useState(0);
+
+	useEffect(() => {
+		let total = 0;
+		let count = 0;
+		txHistory?.forEach((tx) => {
+			total += parseFloat(tx.amount);
+			if (tx.fund_name === fundName) {
+				if (tx.tx_name === "Buy Fund") {
+					count += tx.share_count;
+				} else if (tx.tx_name === "Sell Fund") {
+					count -= tx.share_count;
+				}
+			}
+		});
+		setBalance(total);
+		setShareCount(count);
+	}, [txHistory, fundName]);
+
+	const mostRecentPrice = fund ? fund[fund.length - 1].close_price : 0;
+	const totalPrice = mostRecentPrice * quantity;
+
+	const handleQuantityChange = (e) => {
+		setQuantity(e.target.value);
+	};
+
+	const canBuy = balance >= totalPrice;
+	const canSell = shareCount >= quantity;
 	return (
 		<>
 			<Modal show={show} onHide={handleClose}>
@@ -14,13 +51,30 @@ export default function TransactionModal({
 						{purchase ? `Buy (${fundName})` : `Sell (${fundName})`}
 					</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>Share Count: 0</Modal.Body>
-
-				<Modal.Body>Balance: $0</Modal.Body>
-				<Modal.Body>Current Price: $10</Modal.Body>
-				<Modal.Body>Current Price: $10</Modal.Body>
+				<Modal.Body>
+					<p>Owned Shares: {shareCount}</p>
+					<p>Balance: ${balance}</p>
+					<p>Current Price: ${mostRecentPrice}</p>
+					Qty:{" "}
+					<input
+						type="number"
+						name="quantity"
+						onChange={handleQuantityChange}
+						className="mb-3"
+					/>
+					<p>Total: ${totalPrice}</p>
+				</Modal.Body>
 
 				<Modal.Footer>
+					{purchase ? (
+						<Button variant="success" disabled={!canBuy}>
+							Buy
+						</Button>
+					) : (
+						<Button variant="danger" disabled={!canSell}>
+							Sell
+						</Button>
+					)}
 					<Button variant="secondary" onClick={handleClose}>
 						Close
 					</Button>
--- frontend/src/pages/App/FundPages/OneFundPage.jsx
+++ frontend/src/pages/App/FundPages/OneFundPage.jsx
@@ -1,18 +1,21 @@
-import { useLocation, useParams } from "react-router-dom";
-import debug from "debug";
-import StockChart from "../../../components/Charts/StockChart";
+import { useContext, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { UserContext } from "../../../hooks/UserProvider";
 import { Button, ButtonGroup } from "react-bootstrap";
+import StockChart from "../../../components/Charts/StockChart";
 import TransactionModal from "../../../components/Transaction/TransactionModal";
-import { useState } from "react";
+import debug from "debug";
 
 const log = debug("frontend:OneFundPage");
 
 export default function OneFundPage() {
+	const { user } = useContext(UserContext);
 	const { fundName } = useParams();
 	const location = useLocation();
-	const fund = location.state;
+	const navigate = useNavigate();
 	const [showModal, setShowModal] = useState(false);
 	const [purchase, setPurchase] = useState(true);
+	const fund = location.state;
 
 	fund.sort((a, b) => new Date(a.date) - new Date(b.date));
 	for (let i = 1; i < fund.length; i++) {
@@ -21,7 +24,6 @@ export default function OneFundPage() {
 		const growth = ((currentPrice - previousPrice) / previousPrice) * 100;
 		fund[i].growth = growth.toFixed(2) + "%";
 	}
-	log(fund);
 
 	const handleClick = (e) => {
 		if (e.target.name === "Buy") {
@@ -34,21 +36,28 @@ export default function OneFundPage() {
 
 	return (
 		<div style={{ width: "90%", margin: "0 auto" }}>
+			<h1>{fundName}</h1>
 			<StockChart data={fund} height={400} strokeColor="blue" />
-			<div>{fundName}</div>
-			<ButtonGroup aria-label="Basic example" onClick={handleClick}>
-				<Button variant="success" name="Buy">
-					Buy
+			{user ? (
+				<ButtonGroup aria-label="Basic example" onClick={handleClick}>
+					<Button variant="success" name="Buy">
+						Buy
+					</Button>
+					<Button variant="danger" name="Sell">
+						Sell
+					</Button>
+				</ButtonGroup>
+			) : (
+				<Button variant="warning" onClick={() => navigate("/login")}>
+					Login to trade
 				</Button>
-				<Button variant="danger" name="Sell">
-					Sell
-				</Button>
-			</ButtonGroup>
+			)}
 			<TransactionModal
 				show={showModal}
 				handleClose={() => setShowModal(false)}
 				purchase={purchase}
 				fundName={fundName}
+				fund={fund}
 			/>
 		</div>
 	);
